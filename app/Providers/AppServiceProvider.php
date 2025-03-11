1250367c31e138a7f426ce8c753aba81f8bf1e91
muhammadmuminaziz <muhammadmuminaziz@gmail.com>
2025-03-11 14:56:25 +0700

    add tage meta

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Filament::registerRenderHook('head.start', function () {
+            return '<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">';
+        });
     }
 }
